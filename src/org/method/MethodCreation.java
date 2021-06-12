edc7ba7b008ac3177684c83396c0763ea7d64401
rkrobert68 <rkrobert68@gmail.com>
2021-06-12 08:42:32 +0530

    new project

---
+++ src/org/method/MethodCreation.java
@@ -0,0 +1,31 @@
+package org.method;
+
+public class MethodCreation {
+
+	
+	
+	
+	public void empId() {
+		System.out.println("the employee id is 1826");
+	}
+	public static void main(String[] args) {
+		
+		MethodCreation m=new MethodCreation();
+	m.empId();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
